486b63cc4f0e3422a0844b7a7eb57c71bf7859b3
Ekaterina <ekaglazkovaa@yandex.ru>
2025-02-05 17:01:41 +0300

    small footer fixes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import styles from "./style.module.scss";
 export default function Header() {
   return (
     <footer className={styles.footer}>
-      <span>pet-project made by</span>
+      <span>pet-project was made by</span>
       <a href="https://github.com/Eka-G" className={styles.footer__link}>
         <img src={gitLogo} className={styles.footer__logo} alt="GitHub" />
       </a>
